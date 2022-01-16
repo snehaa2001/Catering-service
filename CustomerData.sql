cf70807499392680e0873bf14326c5683a2858a1
Sneha <89207792+snehaa2001@users.noreply.github.com>
2022-01-16 23:17:14 +0530

    Create CustomerData.sql

---
+++ CustomerData.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CustomerData
+(
+    CustomerName VARCHAR(20),
+    PhoneNumber INTEGER(10),
+    Address VARCHAR(50),
+    Item VARCHAR(50),
+    Bill INTEGER(5),
+    PRIMARY KEY (CustomerName)
+)
